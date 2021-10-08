d422c3ed1e522255ba030c56584ae0a410a2086c
Degemar <degemarpereira@gmail.com>
2021-10-08 15:36:08 -0300

    Terminando o primeiro exercicio

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-primeiros-passos/exercicio.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-primeiros-passos/exercicio.js
@@ -1,3 +1,11 @@
-const myName = "Degemar"
+const myName = "Degemar";
+const birthCity = "Mossoró";
+let birthYear = 1998;
 
-console.log(myName)
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030
+
+console.log(birthYear)
