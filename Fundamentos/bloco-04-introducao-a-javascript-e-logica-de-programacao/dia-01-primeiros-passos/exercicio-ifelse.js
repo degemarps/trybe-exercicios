dc142a6b21aa8a008348edd3ae3d7eea745cf40e
Degemar <degemarpereira@gmail.com>
2021-10-08 16:29:32 -0300

    Terminando o exercicio if-else

---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-primeiros-passos/exercicio-ifelse.js
@@ -0,0 +1,14 @@
+const nota = 90;
+
+if (nota >= 80) {
+  console.log("Parabéns, você foi aprovada(o)!");
+} 
+else if (nota < 80) {
+  console.log("Você está na nossa lista de espera");
+}
+else if (nota < 60) {
+  console.log("Você foi reprovada(o)");
+}
+else {
+  console.log("Valor inválido!");
+}
