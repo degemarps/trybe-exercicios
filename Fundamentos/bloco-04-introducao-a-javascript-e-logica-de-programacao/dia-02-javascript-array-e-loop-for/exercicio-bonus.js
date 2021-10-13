cdc5cfaaffac679bbac2af9f3812bd9d2d118805
Degemar <degemarpereira@gmail.com>
2021-10-13 18:59:07 -0300

    Primeira e segunda parte da atividade bonus finalizadas

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicio-bonus.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicio-bonus.js
@@ -9,3 +9,17 @@ for (let index = 1; index < numbers.length; index += 1) {
     }
   }
 }
+
+console.log(numbers);
+
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] > numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
+  }
+}
+
+console.log(numbers);
