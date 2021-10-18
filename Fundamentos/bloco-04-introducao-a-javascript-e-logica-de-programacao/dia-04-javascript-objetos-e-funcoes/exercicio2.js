cbaddb469d099475aa004df52d8d8258daf3f9f4
Degemar <degemarpereira@gmail.com>
2021-10-18 10:37:53 -0300

    Segunda e terceira parte do exercicio concluidas

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicio2.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicio2.js
@@ -17,3 +17,47 @@ function verifyPalindromo(word) {
 let word = "arara";
 
 console.log(verifyPalindromo(word));
+
+console.log("-------------------------");
+
+function verifyIndexMax(array) {
+  let maior = array[0];
+  let index = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > maior) {
+      maior = array[i];
+      index = i;
+
+    }
+  }
+
+  return index;
+}
+
+let listNumbers = [2, 4, 6, 7, 10, 1];
+
+console.log(verifyIndexMax(listNumbers));
+
+console.log("-------------------------");
+
+function verifyIndexMin(array) {
+  let menor = array[0];
+  let index = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < menor) {
+      menor = array[i];
+      index = i;
+
+    }
+  }
+
+  return index;
+}
+
+let listNumbers2 = [2, 4, 6, 7, 10, 0, -3];
+
+console.log(verifyIndexMin(listNumbers2));
+
+console.log("-------------------------");
