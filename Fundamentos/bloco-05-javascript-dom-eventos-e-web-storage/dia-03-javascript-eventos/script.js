b8451ed6eb4eba2d730c9ad3b474d2e4c6ad63a1
Degemar <degemarpereira@gmail.com>
2021-10-23 16:49:16 -0300

    Parte 1 do exercicio 5.3 feita

--- Fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
+++ Fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
@@ -14,3 +14,32 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+function createDays() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const listDays = document.querySelector('#days');
+
+  for (let i = 0; i < dezDaysList.length; i += 1) {
+    const days = dezDaysList[i];
+    const listItem = document.createElement('li');
+    listItem.innerText = days;
+
+    if ((i - 1) == 24 || (i - 1) == 25 || (i - 1) == 31) {
+      if ((i - 1 == 25)) {
+        listItem.className = 'day holiday friday';
+      } else {
+        listItem.className = 'day holiday';
+      }
+
+    } else if ((i - 1) == 4 || (i - 1) == 11 || (i - 1) == 18) {
+      listItem.className = 'day friday';
+
+    } else {
+      listItem.className = 'day';
+    }
+
+    listDays.appendChild(listItem);
+  }
+}
+
+createDays();
