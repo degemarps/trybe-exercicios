294b5f297950595ec17a245c5b93c7e870ef3cda
Degemar <degemarpereira@gmail.com>
2021-11-20 21:34:31 -0300

    Parte 4 do exercicio 8.2 feita

--- Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise1.js
+++ Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise1.js
@@ -88,19 +88,71 @@ function smallerName(objList) {
 console.log(smallerName(books));
 
 // Encontre o primeiro livro cujo nome possui 26 caracteres.
-const expectedResult = {
-  author: {
-    birthYear: 1948,
-    name: 'George R. R. Martin',
-  },
-  genre: 'Fantasia',
-  id: 1,
-  name: 'As Crônicas de Gelo e Fogo',
-  releaseYear: 1991,
-};
+// const expectedResult = {
+//   author: {
+//     birthYear: 1948,
+//     name: 'George R. R. Martin',
+//   },
+//   genre: 'Fantasia',
+//   id: 1,
+//   name: 'As Crônicas de Gelo e Fogo',
+//   releaseYear: 1991,
+// };
 
 function getNamedBook(objList) {
   return objList.find((obj) => obj.name.length === 26);
 }
 
 console.log(getNamedBook(books));
+
+// Ordene os livros por data de lançamento em ordem decrescente.
+// const expectedResult = [
+//   {
+//     id: 1,
+//     name: 'As Crônicas de Gelo e Fogo',
+//     genre: 'Fantasia',
+//     author: { name: 'George R. R. Martin', birthYear: 1948 },
+//     releaseYear: 1991,
+//   },
+//   {
+//     id: 5,
+//     name: 'A Coisa',
+//     genre: 'Terror',
+//     author: { name: 'Stephen King', birthYear: 1947 },
+//     releaseYear: 1986,
+//   },
+//   {
+//     id: 4,
+//     name: 'Duna',
+//     genre: 'Ficção Científica',
+//     author: { name: 'Frank Herbert', birthYear: 1920 },
+//     releaseYear: 1965,
+//   },
+//   {
+//     id: 2,
+//     name: 'O Senhor dos Anéis',
+//     genre: 'Fantasia',
+//     author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+//     releaseYear: 1954,
+//   },
+//   {
+//     id: 3,
+//     name: 'Fundação',
+//     genre: 'Ficção Científica',
+//     author: { name: 'Isaac Asimov', birthYear: 1920 },
+//     releaseYear: 1951,
+//   },
+//   {
+//     id: 6,
+//     name: 'O Chamado de Cthulhu',
+//     genre: 'Terror',
+//     author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+//     releaseYear: 1928,
+//   },
+// ];
+
+function booksOrderedByReleaseYearDesc(objList) {
+  return objList.sort((objA, objB) => objB.releaseYear - objA.releaseYear);
+}
+
+console.log(booksOrderedByReleaseYearDesc(books));
