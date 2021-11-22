a89e37ae49a7403b72f4612ccb581857e98904a0
Degemar <degemarpereira@gmail.com>
2021-11-22 12:03:15 -0300

    Parte 4 do exercicio 8.3 feita

--- Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
+++ Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
@@ -156,3 +156,35 @@ function fantasyOrScienceFiction() {
 }
 
 console.log(fantasyOrScienceFiction());
+
+// Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
+// const expectedResult = [
+//   {
+//     id: 6,
+//     name: 'O Chamado de Cthulhu',
+//     genre: 'Terror',
+//     author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+//     releaseYear: 1928,
+//   },
+//   {
+//     id: 3,
+//     name: 'Fundação',
+//     genre: 'Ficção Científica',
+//     author: { name: 'Isaac Asimov', birthYear: 1920 },
+//     releaseYear: 1951,
+//   },
+//   {
+//     id: 2,
+//     name: 'O Senhor dos Anéis',
+//     genre: 'Fantasia',
+//     author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+//     releaseYear: 1954,
+//   },
+// ];
+
+function oldBooksOrdered() {
+  const olderBooks = books.filter((book) => 2021 - book.releaseYear > 60);
+  return olderBooks.sort((a, b) => a.releaseYear - b.releaseYear);
+}
+
+console.log(oldBooksOrdered());
