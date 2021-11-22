3aee501a39c50992210685ac5922f81bdfaa56de
Degemar <degemarpereira@gmail.com>
2021-11-22 13:00:20 -0300

    Finalizando o exercicio 8.3

--- Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
+++ Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
@@ -218,3 +218,14 @@ function oldBooks() {
 }
 
 console.log(oldBooks());
+
+// Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+// const expectedResult = 'O Senhor dos Anéis';
+
+function authorWith3DotsOnName() {
+  return books.find((book) => (
+    book.author.name.split(' ').filter((word) => word.endsWith('.')).length === 3
+  )).name;
+}
+
+console.log(authorWith3DotsOnName());
