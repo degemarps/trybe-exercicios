b22ea1edb00e71800c31bbb63a20dc8b015e81d5
Degemar <degemarpereira@gmail.com>
2021-11-29 22:07:58 -0300

    Parte 6 do exercicio 9.1 feita

--- Fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicios1.js
+++ Fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicios1.js
@@ -128,3 +128,6 @@ sendMarsTemperature(temperatureInFahrenheit, handleError);
 
 // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
 sendMarsTemperature(greet, handleError);
+
+//  Escreva um teste que verifique a chamada do callback de uma função uppercase , que transforma as letras de uma palavra em letras maiúsculas. 
+// Lembre-se de ter cuidado com os falso-positivos em testes assíncronos.
