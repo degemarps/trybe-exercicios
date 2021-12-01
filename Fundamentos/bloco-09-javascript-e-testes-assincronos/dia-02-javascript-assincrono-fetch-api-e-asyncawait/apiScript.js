c233ee03febea79a94b6a3eba776b62fbc054d07
Degemar <degemarpereira@gmail.com>
2021-11-30 22:24:55 -0300

    Parte 1 do exercicio 9.2 feito

--- Fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyncawait/apiScript.js
+++ Fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyncawait/apiScript.js
@@ -2,7 +2,20 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  }
+
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => {
+      const jokeContainer = document.getElementById('jokeContainer');
+      const paragraph = document.createElement('p');
+      paragraph.innerText = data.joke;
+      jokeContainer.appendChild(paragraph);
+    });
 };
 
 window.onload = () => fetchJoke();
