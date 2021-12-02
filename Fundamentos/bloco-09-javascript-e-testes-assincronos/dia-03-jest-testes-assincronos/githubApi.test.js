20d230a07d2dabe05af2ad2811d74aecd6b6478c
Degemar <degemarpereira@gmail.com>
2021-12-01 22:02:18 -0300

    Terminando o exercicio 9.3

--- Fundamentos/bloco-09-javascript-e-testes-assincronos/dia-03-jest-testes-assincronos/githubApi.test.js
+++ Fundamentos/bloco-09-javascript-e-testes-assincronos/dia-03-jest-testes-assincronos/githubApi.test.js
@@ -1,5 +1,6 @@
 // githubApi.test.js
 
+const { not } = require('micromatch');
 const fetch = require('node-fetch');
 
 const getRepos = (url) => (
@@ -8,4 +9,13 @@ const getRepos = (url) => (
     .then((data) => data.map((repo) => repo.name))
 );
 
-// implemente seus testes aqui
+describe('Teste a resposta do github', () => {
+  test('Verifique se a resposta contém os repositórios indicados', async () => {
+    const url = 'https://api.github.com/orgs/tryber/repos';
+
+    const response = await getRepos(url);
+
+    expect(response).toContain('sd-01-week4-5-project-todo-list');
+    expect(response).toContain('sd-01-week4-5-project-meme-generator');
+  });
+}); 
