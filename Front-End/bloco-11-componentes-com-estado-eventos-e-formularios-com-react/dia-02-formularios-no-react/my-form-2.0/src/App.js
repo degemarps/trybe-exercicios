bb886f72be6e3556068fae21552fd9ce8c80f656
degemarps <degemarpereira@gmail.com>
2022-01-06 21:56:24 -0300

    Parte inicial do exercicio 11.2

---
+++ Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/my-form-2.0/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import PersonalData from './PersonalData';
+import LastJob from './LastJob';
+
+function App() {
+  return (
+    <form>
+      <PersonalData />
+      <LastJob />
+    </form>
+  );
+}
+
+export default App;
---
+++ Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/my-form-2.0/src/LastJob.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './LastJob.css'
+
+class LastJob extends React.Component {
+  render() {
+    return (
+      <fieldset className="container">
+        <label>
+          Resumo do Currículo
+          <textarea maxLength="1000" name="curriculo" required />
+        </label>
+
+        <label>
+          Cargo
+          <textarea maxLength="40" name="cargi" required />
+        </label>
+
+        <label>
+          Descrição do Cargo
+          <input type="text" maxLength="500" name="descricao-cargo" required />
+        </label>
+      </fieldset>
+    );
+  }
+}
+
+export default LastJob;
