2831a355cd2e348517b6eee2143ff107807aab79
degemarps <degemarpereira@gmail.com>
2022-04-23 12:46:09 -0300

    Exercício 22.2 async/await

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/index.js
@@ -1,4 +1,5 @@
-function matematicFunc(num1, num2, num3) {
+// Utilizando then/catch
+function matematicFunc1(num1, num2, num3) {
 	const promise = new Promise((resolve, reject) => {
 		if (typeof num1 !== 'number'
       || typeof num2 !== 'number'
@@ -15,6 +16,35 @@ function matematicFunc(num1, num2, num3) {
 	return promise;
 }
 
-matematicFunc(3, 5, 9)
+matematicFunc1(3, 5, 9)
 	.then(result => console.log(`Sucesso: ${result}`))
 	.catch(err => console.log(`erro: ${err}`));
+
+// Utilizando async/await
+function matematicFunc2(num1, num2, num3) {
+  const promise = new Promise((resolve, reject) => {
+    if (typeof num1 !== 'number'
+      || typeof num2 !== 'number'
+      || typeof num3 !== 'number')
+      reject(new Error('Informe apenas números')); 
+    
+    const resultOp = (num1 + num2) * num3;
+
+    if (resultOp < 50) reject(new Error('Valor muito baixo'));
+
+    resolve(resultOp)
+  });
+
+  return promise;
+}
+
+async function resolveFunc() {
+  try {
+    const result = await matematicFunc2(3, 5, 6);
+    console.log(`Sucesso: ${result}`);
+  } catch (error) {
+    console.log(`erro: ${error}`);
+  }
+}
+
+resolveFunc();
