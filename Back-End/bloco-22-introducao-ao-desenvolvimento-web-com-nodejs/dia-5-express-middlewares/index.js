3cbe4b2b5150bd25f802c00bf019bcadca9bedc4
degemarps <degemarpereira@gmail.com>
2022-05-03 11:51:30 -0300

    Validação do product name

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const validateProductName = require('./utils/validateProductName');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,6 +10,8 @@ const PORT = 4000;
 app.get('/', (req, res) => {
   res.status(200).json({ message: "Hello World" });
 });
+
+app.post('/sales', validateProductName, (req, res) => {});
 
 app.listen(PORT, () => {
   console.log('Online');
