ed560352addf3a6e6faf437149ca73cd5644d4c7
degemarps <degemarpereira@gmail.com>
2022-05-18 12:56:34 -0300

    Mensagem de erro quando um livro não é encontrado

--- Back-End/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/controllers/booksController.js
+++ Back-End/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/controllers/booksController.js
@@ -10,6 +10,8 @@ const getById = async (req, res) => {
   const { id } = req.params;
   const book = await bookService.getById(id);
 
+  if (!book) return res.status(404).json({ message: 'Book not found' });
+
   return res.status(200).json(book);
 };
 
