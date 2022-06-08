848119d23f2cef975f99d3fc2df664628c6a9166
degemarps <degemarpereira@gmail.com>
2022-06-08 09:51:00 -0300

    parte 1 e 2 do exercício 26.1

---
+++ Back-End/bloco-26-typescript/dia-1-introducao-ao-typescript/dist/exercise2.js
@@ -0,0 +1,18 @@
+"use strict";
+var yearMonths;
+(function (yearMonths) {
+    yearMonths["JANEIRO"] = "Janeiro";
+    yearMonths["FEVEREIRO"] = "Fevereiro";
+    yearMonths["MARCO"] = "Mar\u00E7o";
+    yearMonths["ABRIL"] = "Abril";
+    yearMonths["MAIO"] = "Maio";
+    yearMonths["JUNHO"] = "Junho";
+    yearMonths["JULHO"] = "Julho";
+    yearMonths["AGOSTO"] = "Agosto";
+    yearMonths["SETEMBRO"] = "Setembro";
+    yearMonths["OUTUBRO"] = "Outubro";
+    yearMonths["NOVEMBRO"] = "Novembro";
+    yearMonths["DEZEMBRO"] = "Dezembro";
+})(yearMonths || (yearMonths = {}));
+;
+console.log(yearMonths);
---
+++ Back-End/bloco-26-typescript/dia-1-introducao-ao-typescript/exercise2.ts
@@ -0,0 +1,17 @@
+enum yearMonths {
+  JANEIRO = 'Janeiro',
+  FEVEREIRO = 'Fevereiro',
+  MARCO = 'Março',
+  ABRIL = 'Abril',
+  MAIO = 'Maio',
+  JUNHO = 'Junho',
+  JULHO = 'Julho',
+  AGOSTO = 'Agosto',
+  SETEMBRO = 'Setembro',
+  OUTUBRO = 'Outubro',
+  NOVEMBRO = 'Novembro',
+  DEZEMBRO = 'Dezembro',
+};
+
+console.log(yearMonths);
+
