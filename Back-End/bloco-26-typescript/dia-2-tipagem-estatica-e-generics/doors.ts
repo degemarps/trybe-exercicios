6b1725f978e7cf0ae0b1bdc5c1c7dac564c70d30
degemarps <degemarpereira@gmail.com>
2022-06-09 13:02:36 -0300

    Adição do enum doors

---
+++ Back-End/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/doors.ts
@@ -0,0 +1,8 @@
+enum Doors {
+  driver = "da pessoa motorista",
+  ride = "da pessoa carona",
+  behind_driver = "de trás da pessoa motorista",
+  behind_ride = "de trás da pessoa carona"
+}
+
+export default Doors;
